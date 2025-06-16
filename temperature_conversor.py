bdd0819950de562de866327037a75206655ad0e5
PalomaTavares <palomafernandastavares@gmail.com>
2025-06-16 20:47:57 -0300

    fix: formulas erradas

--- temperature_conversor.py
+++ temperature_conversor.py
@@ -3,11 +3,11 @@ class TemperatureConversor:
     def celsius_to_fahrenheit(celsius):
         return (celsius * 9/5) + 32
     def fahrenheit_to_celsius(fahrenheit):
-        return (fahrenheit - 32) * 9/5
+        return (fahrenheit - 32) * 5/9
     
     #celsius and kelvin
     def celsius_to_kelvin(celsius):
-        return + 273.15
+        return celsius+ 273.15
     def kelvin_to_celsius(kelvin):
         return kelvin - 273.15
     
@@ -15,4 +15,4 @@ class TemperatureConversor:
     def kelvin_to_fahrenheit(kelvin):
         return (kelvin - 273.15) * 9/5 + 32
     def fahrenheit_to_kelvin(fahrenheit):
-        return (fahrenheit - 32) * 9/5 + 273.15
+        return (fahrenheit - 32) * 5/9 + 273.15
